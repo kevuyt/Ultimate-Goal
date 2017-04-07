fe2c04e838913d94b41f17a08d7757adab7b0a6d
archishou <archishou@gmail.com>
2017-04-07 15:43:07 -0400

    Updates to autos

--- Masquerade4997Code/src/main/java/org/firstinspires/ftc/teamcode/Autonomus/BlueAuto.java
+++ Masquerade4997Code/src/main/java/org/firstinspires/ftc/teamcode/Autonomus/BlueAuto.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 import Library4997.MasqLinearOpMode;
 import Library4997.MasqRobot.Direction;
+import Library4997.MasqRobot.MasqRobot;
 
 /**
  * BlueAuto
@@ -12,14 +13,27 @@ import Library4997.MasqRobot.Direction;
 
 @Autonomous(name = "BlueAuto", group = "Blue")
 public class BlueAuto extends MasqLinearOpMode implements Constants {
+    private int delay = 0;
     public void runLinearOpMode() throws InterruptedException {
         while (!opModeIsActive()) {
+            dash.create("Press A on gamepad1 to set a delay of one second press b to reset");
+            if (controller1.apr()){
+                delay += 1000;
+            }
+            else if (controller1.bpr()){
+                delay = 0;
+            }
+            dash.create("DELAY: ", delay);
             dash.create(robot.imu);
-            dash.create(robot.leftColor);
-
+            dash.createSticky(robot.imu);
             dash.update();
         }
         waitForStart();
-        robot.drive(60);
+        robot.setAllianceColor(MasqRobot.AllianceColor.BLUE);
+        robot.sleep(delay);
+        robot.drive(10);
+        robot.turn(48, Direction.LEFT);
+        robot.drive(75);
+
     }
 }
