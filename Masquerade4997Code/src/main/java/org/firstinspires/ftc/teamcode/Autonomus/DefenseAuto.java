84b26d971b4485a17b1a95f548d07b8afac74cc7
archishou <archishou@gmail.com>
2017-04-12 16:27:56 -0400

    finishing up on auto for scrimmage

--- Masquerade4997Code/src/main/java/org/firstinspires/ftc/teamcode/Autonomus/DefenseAuto.java
+++ Masquerade4997Code/src/main/java/org/firstinspires/ftc/teamcode/Autonomus/DefenseAuto.java
@@ -16,22 +16,19 @@ public class DefenseAuto extends MasqLinearOpMode implements Constants {
     private int delay = 0;
     public void runLinearOpMode() throws InterruptedException {
         while (!opModeIsActive()) {
-            if (controller1.apr()){
-                delay += 1000;
-            }
-            dash.create("DELAY: ", delay);
             dash.create(robot.imu);
-            dash.createSticky(robot.imu);
             dash.update();
         }
         waitForStart();
         robot.sleep(delay);
-        robot.drive(10);
+        robot.drive(30);
         robot.shooter.setPower(-1);
         robot.sleep(1000);
         robot.indexer.setPosition(INDEXER_OPENED);
         robot.turn(43, Direction.LEFT);
         robot.drive(270);
+        robot.sleep();
+        robot.drive(50, POWER_OPTIMAL, Direction.BACKWARD);
         
 
 
