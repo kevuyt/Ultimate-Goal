13dfe30b1ac848964fb6c314cadd794995c7b78a
archishou <archishou@gmail.com>
2017-04-15 14:26:33 -0400

    Updated Template also 200 COMMITS WEEE

--- Masquerade4997Code/src/main/java/org/firstinspires/ftc/teamcode/Autonomus/Template.java
+++ Masquerade4997Code/src/main/java/org/firstinspires/ftc/teamcode/Autonomus/Template.java
@@ -16,19 +16,9 @@ public class Template extends MasqLinearOpMode implements Constants {
     private int delay = 0;
     public void runLinearOpMode() throws InterruptedException {
         while (!opModeIsActive()) {
-            dash.create("Press A on gamepad1 to set a delay of one second press b to reset");
-            if (controller1.apr()){
-                delay += 1000;
-            }
-            else if (controller1.bpr()){
-                delay = 0;
-            }
-            dash.create("DELAY: ", delay);
             dash.create(robot.imu);
-            dash.createSticky(robot.imu);
             dash.update();
         }
         waitForStart();
-        robot.sleep(delay);
     }
 }
