d252ecad451c9a4e15aaa45e34319f1ea6274222
Archishmaan Peyyety <archishou@gmail.com>
2017-07-14 15:17:11 -0400

    Set up auto classes

--- Masquerade4997Code/src/main/java/org/firstinspires/ftc/teamcode/Autonomus/RelicMainAuto.java
+++ Masquerade4997Code/src/main/java/org/firstinspires/ftc/teamcode/Autonomus/RelicMainAuto.java
@@ -1,8 +1,22 @@
 package org.firstinspires.ftc.teamcode.Autonomus;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+
+import Library4997.MasqWrappers.MasqLinearOpMode;
+
 /**
- * Created by Archish on 7/14/17.
+ * This is a basic template copy and paste this class for any auto,
+ * refactor the file name to match the auto class title
  */
 
-public class RelicMainAuto {
+@Autonomous(name = "MainAuto", group = "Auto")
+public class RelicMainAuto extends MasqLinearOpMode implements Constants {
+    public void runLinearOpMode() throws InterruptedException {
+        while (!opModeIsActive()) {
+            dash.create(robot.imu);
+            dash.update();
+        }
+        waitForStart();
+    }
 }
