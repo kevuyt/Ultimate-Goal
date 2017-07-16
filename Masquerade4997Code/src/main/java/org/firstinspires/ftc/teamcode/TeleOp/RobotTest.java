4d475a5077eae8261c836c4988567daf7f68a7b0
Archishmaan Peyyety <archishou@gmail.com>
2017-07-16 11:00:11 -0400

    Robot two teleop is uptodate

--- Masquerade4997Code/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/RobotTest.java
+++ Masquerade4997Code/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/RobotTest.java
@@ -1,17 +1,23 @@
 package org.firstinspires.ftc.teamcode.TeleOp;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import Library4997.MasqMotors.MasqTankDrive;
 
 /**
  * Created by Archish on 7/15/17.
  */
-
+@TeleOp(name="R2", group="Final")
 public class RobotTest extends LinearOpMode {
     public MasqTankDrive robot = new MasqTankDrive("leftFront", "leftBack", "rightBack", "rightFront");
     @Override
     public void runOpMode() throws InterruptedException {
+        while (!isStarted()) {
+            telemetry.addLine("Hello Faggot");
+            telemetry.update();
+        }
         while (opModeIsActive()) {
             float move = -gamepad1.left_stick_y;
             float turn = -gamepad1.right_stick_x;
