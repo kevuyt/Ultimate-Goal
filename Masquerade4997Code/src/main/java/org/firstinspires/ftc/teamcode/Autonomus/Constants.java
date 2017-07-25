b559340b8514a9600bfb6c81fc78a0e532cd17af
Archishmaan Peyyety <archishou@gmail.com>
2017-07-24 20:02:40 -0400

    Edited constants

--- Masquerade4997Code/src/main/java/org/firstinspires/ftc/teamcode/Autonomus/Constants.java
+++ Masquerade4997Code/src/main/java/org/firstinspires/ftc/teamcode/Autonomus/Constants.java
@@ -8,10 +8,9 @@ public interface Constants {
     double POWER_LOW = 0.3,
            POWER_OPTIMAL = 0.5,
            POWER_HIGH = 0.7;
-    double SLEEPTIME_HIGH = 3000,
-           SLEEPTIME_OPTIMAL = 1000,
-           SLEEPTIME_LOW = 500;
-
+    double SLEEPTIME_HIGH = 1000,
+           SLEEPTIME_OPTIMAL = 500,
+           SLEEPTIME_LOW = 250;
     double COLLECTOR_IN = -1.5;
     double COLLECTOR_OUT = 1.5;
 }
