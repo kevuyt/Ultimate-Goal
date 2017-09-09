e297df9cb4f195841523c4c5962c18d6978248d7
Archishmaan Peyyety <archishou@gmail.com>
2017-09-09 18:55:55 -0400

    Delete all old constants

--- Masquerade4997Code/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Constants.java
+++ Masquerade4997Code/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Constants.java
@@ -5,18 +5,5 @@ package org.firstinspires.ftc.teamcode.TeleOp;
  */
 
 public interface Constants {
-    double INDEXER_CLOSED = 0;
-    double INDEXER_OPENED = 0.6;
 
-    double TARGET_POWER = -0.8;
-    double REV_UP = 0.1;
-    double REV_DOWN = 0.01;
-    double LOW_POWER_FACTOR = 0.2;
-    double SHOOTER_ERROR = 0.05;
-
-    double BEACON_OUT = -1;
-    double BEACON_IN = 1;
-
-    double COLLECTOR_IN = -1.5;
-    double COLLECTOR_OUT = 1.5;
 }
