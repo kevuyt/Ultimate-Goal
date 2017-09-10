5f57ed7e84453e494661fe54b9c42072bba6a1af
Archishmaan Peyyety <archishou@gmail.com>
2017-09-10 05:34:38 -0400

    Cleaned ScaleInput Method @theforkofyork

--- Masquerade4997Library/src/main/java/Library4997/MasqRobot.java
+++ Masquerade4997Library/src/main/java/Library4997/MasqRobot.java
@@ -396,16 +396,14 @@ public class MasqRobot implements PID_CONSTANTS {
     public void sleep() {
         sleep(DEFAULT_SLEEP_TIME);
     }
-    public double getDelay(){
-        return FtcRobotControllerActivity.getDelay();
-    }
+    public double getDelay(){return FtcRobotControllerActivity.getDelay();}
     private double scaleInput(double d)  {
         double[] scaleArray = { 0.0, 0.05, 0.09, 0.10, 0.12, 0.15, 0.18, 0.24,
                 0.30, 0.36, 0.43, 0.50, 0.60, 0.72, 0.85, 1.00, 1.00 };
         int index = (int) (d * 16.0);
-        if (index < 0) {index = -index;}
+        index = Math.abs(index);
         if (index > 16) {index = 16;}
-        double dScale = 0.0;
+        double dScale;
         if (d < 0) {dScale = -scaleArray[index];}
         else {dScale = scaleArray[index];}
         return dScale;
