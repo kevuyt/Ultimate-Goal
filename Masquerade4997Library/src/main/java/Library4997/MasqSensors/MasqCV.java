9ff4ec1dcd2d63cfe02cded5931e6130f0b99ebf
Archishmaan Peyyety <archishou@gmail.com>
2017-09-10 07:22:25 -0400

    Started on opencv

--- Masquerade4997Library/src/main/java/Library4997/MasqSensors/MasqCV.java
+++ Masquerade4997Library/src/main/java/Library4997/MasqSensors/MasqCV.java
@@ -5,4 +5,47 @@ package Library4997.MasqSensors;
  */
 
 public class MasqCV {
+    String targetOne, targetTwo, targetThree, asset;
+    public MasqCV (String t1, String t2, String t3, String asset){
+        targetOne = t1;
+        targetTwo = t2;
+        targetThree = t3;
+        this.asset = asset;
+    }
+    public MasqCV (String t1, String t2, String asset){
+        targetOne = t1;
+        targetTwo = t2;
+        targetThree = null;
+        this.asset = asset;
+    }
+    public MasqCV (String t1, String asset){
+        targetOne = t1;
+        targetTwo = null;
+        targetThree = null;
+        this.asset = asset;
+    }
+    public int[] rbg (){
+        int[] rbgreturn = new int[]{0};
+        return rbgreturn;
+    }
+    public boolean isSeen(String target){
+        return true;
+    }
+    public String getTargetSeen(){
+        if (isSeen(targetOne)){
+            return targetOne;
+        } else if (isSeen(targetTwo) && targetTwo != null){
+            return targetTwo;
+        } else if (isSeen(targetThree) && targetThree != null){
+            return targetThree;
+        } else {
+            return null;
+        }
+    }
+    private void findInAssets(){
+
+    }
+    private void pdf2jpeg(){
+
+    }
 }
