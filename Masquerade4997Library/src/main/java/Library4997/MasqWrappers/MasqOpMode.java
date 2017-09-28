9a0596ca9073efce8e38b646ff301b394bb9cabc
Archishmaan Peyyety <archishou@gmail.com>
2017-09-27 21:20:07 -0400

    forced to use hardware maps i will figure out a way around this

--- Masquerade4997Library/src/main/java/Library4997/MasqWrappers/MasqOpMode.java
+++ Masquerade4997Library/src/main/java/Library4997/MasqWrappers/MasqOpMode.java
@@ -9,7 +9,7 @@ import Library4997.MasqRobot;
  */
 
 public abstract class MasqOpMode extends OpMode {
-    public MasqRobot robot = new MasqRobot();
+    public MasqRobot robot = new MasqRobot(hardwareMap);
     public DashBoard dash = new DashBoard(super.telemetry);
     protected MasqController controller1 = new MasqController (super.gamepad1, "controller1");
     protected MasqController controller2 = new MasqController (super.gamepad2, "controller2");
