b41fc2c40ce5dc1ed8d90d574689b3cc950ef1e9
Archishmaan Peyyety <archishou@gmail.com>
2017-10-12 12:16:54 -0400

    new controller

---
+++ Masquerade4997Library/src/main/java/Library4997/MasqWrappers/MasqControllerV2.java
@@ -0,0 +1,121 @@
+package Library4997.MasqWrappers;
+
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+/**
+ * Created by Archish on 10/12/17.
+ */
+
+public class MasqControllerV2 {
+    private String name;
+    private Gamepad gamepad;
+    private boolean
+            aPrev = false, bPrev = false, xPrev = false, yPrev = false,
+            leftBumperPrev = false, rightBumperPrev = false;
+
+    public MasqControllerV2(Gamepad g, String name){
+        this.name = name;
+        this.gamepad = g;
+    }
+
+    public boolean a() {return gamepad.a;}
+    public boolean x() {return gamepad.x;}
+    public boolean y() {return gamepad.y;}
+    public boolean b() {return gamepad.b;}
+
+    public boolean aOnPress() {return a() && !aPrev;}
+    public boolean bOnPress() {return b() && !bPrev;}
+    public boolean yOnPress() {return y() && !yPrev;}
+    public boolean xOnPress() {return x() && !xPrev;}
+
+    public float leftStickX(){return gamepad.left_stick_x;}
+    public float leftStickY() {
+        return gamepad.left_stick_y;
+    }
+    public float rightStickX() {
+        return gamepad.right_stick_x;
+    }
+    public float rightStickY() {
+        return gamepad.right_stick_y;
+    }
+
+    public boolean dpad_up() {
+        return gamepad.dpad_up;
+    }
+    public boolean dpad_down() {
+        return gamepad.dpad_down;
+    }
+    public boolean dpad_left() {
+        return gamepad.dpad_left;
+    }
+    public boolean dpad_right() {
+        return gamepad.dpad_right;
+    }
+
+    public boolean leftBumper() {
+        return gamepad.left_bumper;
+    }
+    public boolean rightBumper() {
+        return gamepad.right_bumper;
+    }
+
+    public boolean leftBumperOnPress () {return leftBumper() && !leftBumperPrev;}
+    public boolean rightBumperOnPress () {return rightBumper() && !rightBumperPrev;}
+
+    public boolean leftStickButton() {
+        return gamepad.left_stick_button;
+    }
+    public boolean rightStickButton() {
+        return gamepad.right_stick_button;
+    }
+
+    public boolean leftTriggerPressed() {
+        return leftTrigger() > 0;
+    }
+    public boolean rightTriggerPressed() {
+        return rightTrigger() > 0;
+    }
+
+    public float leftTrigger() {return gamepad.left_trigger;}
+    public float rightTrigger() {return gamepad.right_trigger;}
+
+    public void update(){
+        aPrev = gamepad.a;
+        bPrev = gamepad.b;
+        xPrev = gamepad.x;
+        yPrev = gamepad.y;
+        leftBumperPrev = gamepad.left_bumper;
+        rightBumperPrev = gamepad.right_bumper;
+    }
+
+    public String getName() {return name;}
+    public String[] getDash() {
+        return new String[]{
+                name,
+                "A: " + Boolean.toString(a()),
+                "aPress: " + Boolean.toString(aOnPress()),
+                "B: " + Boolean.toString(b()),
+                "bPress: " + Boolean.toString(bOnPress()),
+                "X: " + Boolean.toString(x()),
+                "xPress: " + Boolean.toString(xOnPress()),
+                "Y: " + Boolean.toString(y()),
+                "yPress: " + Boolean.toString(yOnPress()),
+                "RB: " + Boolean.toString(rightBumper()),
+                "rightBumperPressed: " + Boolean.toString(rightBumperOnPress()),
+                "LB: " + Boolean.toString(leftBumper()),
+                "leftBumperPressed: " + Boolean.toString(leftBumperOnPress()),
+                "leftTrigger: " + Float.toString(leftTrigger()),
+                "leftTriggerPress: " + Boolean.toString(leftTriggerPressed()),
+                "rightTrigger: " + Float.toString(rightTrigger()),
+                "rightTriggerPress: " + Boolean.toString(rightTriggerPressed()),
+                "rightStickY: " + Double.toString(rightStickX()),
+                "rightStickX: " + Double.toString(rightStickX()),
+                "leftStickY: " + Double.toString(leftStickY()),
+                "leftStickX: " + Double.toString(leftStickX()),
+                "D_UP: " + Boolean.toString(dpad_up()),
+                "D_DOWN: " + Boolean.toString(dpad_down()),
+                "D_LEFT: " + Boolean.toString(dpad_left()),
+                "D_RIGHT: " + Boolean.toString(dpad_right()),
+        };
+    }
+}
