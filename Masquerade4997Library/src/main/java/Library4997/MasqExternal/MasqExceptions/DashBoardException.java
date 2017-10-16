81527c701d63fbde1f2a574844ff603c5b3460f4
Archishmaan Peyyety <archishou@gmail.com>
2017-10-16 18:25:29 -0400

    my first comment

--- Masquerade4997Library/src/main/java/Library4997/MasqExternal/MasqExceptions/DashBoardException.java
+++ Masquerade4997Library/src/main/java/Library4997/MasqExternal/MasqExceptions/DashBoardException.java
@@ -5,7 +5,7 @@ import Library4997.MasqExternal.MasqHardware;
 /**
  * Created by Archish on 10/10/17.
  */
-
+//This class was just for fun, it has no real purpose
 public class DashBoardException extends Exception {
     public DashBoardException (MasqHardware hardware){
         super(hardware.getName() + "failed to have dash");
