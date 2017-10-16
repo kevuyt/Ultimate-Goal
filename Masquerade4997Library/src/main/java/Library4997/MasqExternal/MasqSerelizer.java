34901217fc23de547bf400363344b23e36dd2a6e
Archishmaan Peyyety <archishou@gmail.com>
2017-10-15 20:51:00 -0400

    file creation

--- Masquerade4997Library/src/main/java/Library4997/MasqExternal/MasqSerelizer.java
+++ Masquerade4997Library/src/main/java/Library4997/MasqExternal/MasqSerelizer.java
@@ -13,70 +13,42 @@ import java.io.OutputStreamWriter;
  * Created by Archish on 10/15/17.
  */
 
-public class MasqSerelizer {
-    public static String getFileValue(String fileName, Context context) {
+public class MasqSerelizer implements Runnable {
+    private boolean close = false;
+    private MasqHardware hardware;
+    public void startFileWriting(){
+        new Thread(this).start();
+    }
+    public void setHardwareTracking(MasqHardware hardware){
+        this.hardware = hardware;
+    }
+    private void createFile(){
+
+    }
+    private void writeToFile(){
+
+    }
+    public String[] readFromFile(){
+
+    }
+    private void update () {
+
+    }
+    @Override
+    public void run() {
+        boolean close = false;
+        while (!close) {
+            update();
+            close = this.close;
+            sleep();
+        }
+    }
+    public void close() {close = true;}
+    private void sleep(){
         try {
-            StringBuffer outStringBuf = new StringBuffer();
-            String inputLine = "";
-            /*
-             * We have to use the openFileInput()-method the ActivityContext
-             * provides. Again for security reasons with openFileInput(...)
-             */
-            FileInputStream fIn = context.openFileInput(fileName);
-            InputStreamReader isr = new InputStreamReader(fIn);
-            BufferedReader inBuff = new BufferedReader(isr);
-            while ((inputLine = inBuff.readLine()) != null) {
-                outStringBuf.append(inputLine);
-                outStringBuf.append("\n");
-            }
-            inBuff.close();
-            return outStringBuf.toString();
-        } catch (IOException e) {
-            return null;
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
-    }
-
-    public static boolean appendFileValue(String fileName, String value,
-                                          Context context) {
-        return writeToFile(fileName, value, context, Context.MODE_APPEND);
-    }
-
-    public static boolean setFileValue(String fileName, String value,
-                                       Context context) {
-        return writeToFile(fileName, value, context,
-                Context.MODE_WORLD_READABLE);
-    }
-
-    public static boolean writeToFile(String fileName, String value,
-                                      Context context, int writeOrAppendMode) {
-        // just make sure it's one of the modes we support
-        if (writeOrAppendMode != Context.MODE_WORLD_READABLE
-                && writeOrAppendMode != Context.MODE_WORLD_WRITEABLE
-                && writeOrAppendMode != Context.MODE_APPEND) {
-            return false;
-        }
-        try {
-            /*
-             * We have to use the openFileOutput()-method the ActivityContext
-             * provides, to protect your file from others and This is done for
-             * security-reasons. We chose MODE_WORLD_READABLE, because we have
-             * nothing to hide in our file
-             */
-            FileOutputStream fOut = context.openFileOutput(fileName,
-                    writeOrAppendMode);
-            OutputStreamWriter osw = new OutputStreamWriter(fOut);
-            // Write the string to the file
-            osw.write(value);
-            // save and close
-            osw.flush();
-            osw.close();
-        } catch (IOException e) {
-            return false;
-        }
-        return true;
-    }
-
-    public static void deleteFile(String fileName, Context context) {
-        context.deleteFile(fileName);
     }
 }
