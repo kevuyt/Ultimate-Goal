de1370b6b7ebe454bdf86f81a8b707d8138d0bdd
Archishmaan Peyyety <archishou@gmail.com>
2017-10-17 06:34:35 -0400

    add all constants

--- Masquerade4997Library/src/main/java/Library4997/MasqExternal/MasqExternal.java
+++ Masquerade4997Library/src/main/java/Library4997/MasqExternal/MasqExternal.java
@@ -5,6 +5,22 @@ package Library4997.MasqExternal;
  */
 
 public class MasqExternal {
+
+    public static final double MAX_RATE = 3100;
+    public static final double TICKS_PER_ROTATION = 1120;
+    public static final double wheelDiameter = 4;
+    public static final double cmToInches = 2.54;
+    public static final double gearRatio = 1;
+    public static final double CLICKS_PER_CM = ((TICKS_PER_ROTATION / (wheelDiameter * cmToInches)) / Math.PI) / gearRatio;
+
+    public static double ODS_WHITE = 0.7,
+            ODS_BLACK = 0.3;
+    public static String VUFORIA_KEY = "AQL5v9v/////AAAAGey79Q2fZ0i7tLgjrpd85rZwqcK1HlVOI6UUmT02C7slX9+x5Qq" +
+            "CfEwQhnuuB1hOh//uL2LnHYMViBgZtdjDGvmWvDvgKaonymopd0Y62ls2ZJfHhJ3fZYhF57Ce6ZepRI" +
+            "FOumys4J4DssG83OT+DJUjUCG6ruZ88AYjxNzi+vhkTCxHVULQxLJCSQ7boG0t36RWIEmVwxXIHVI" +
+            "3xbVeXwQL7vgm/0KmGW/KJFOuI2+wl1IDJdzDQHfavEA8FFkYTlnp/chHMbLu//BaqXprFHZ6OLh" +
+            "OZoRWiFkg1N0zabreTxMNOYFP/rDNaYseXQVGGRSMHxF86kGs6LNHEO7qZZj/BU94zKpPMWyHYw29X477";
+
     public static void sleep (int sleep) {
         try {Thread.sleep(sleep);}
         catch (InterruptedException e) {e.printStackTrace();}
