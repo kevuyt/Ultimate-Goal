05a89d48d4868db8608e04b2174140995a820fe2
Archishmaan Peyyety <archishou@gmail.com>
2017-10-28 11:16:54 -0400

    distances

--- Masquerade4997Code/src/main/java/org/firstinspires/ftc/teamcode/Autonomus/Constants.java
+++ Masquerade4997Code/src/main/java/org/firstinspires/ftc/teamcode/Autonomus/Constants.java
@@ -11,6 +11,11 @@ public interface Constants extends org.firstinspires.ftc.teamcode.TeleOp.Constan
     double SLEEPTIME_HIGH = 1000,
            SLEEPTIME_OPTIMAL = 500,
            SLEEPTIME_LOW = 250;
+    double CRYPTOBOX_SEPERATION_DISTANCE = 7.63; //this is in inches.
+    double DISTANCE_TO_LEFT_BOX = 36 - (CRYPTOBOX_SEPERATION_DISTANCE / 2),
+           DISTANCE_TO_RIGHT_BOX = 36 + (CRYPTOBOX_SEPERATION_DISTANCE / 2) + CRYPTOBOX_SEPERATION_DISTANCE,
+           DISTANCE_TO_CENTER_BOX = 36 + (CRYPTOBOX_SEPERATION_DISTANCE / 2);
+
     String LEFT = "LEFT",
            RIGHT = "RIGHT",
            CENTER = "CENTER";
