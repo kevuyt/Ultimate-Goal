00b00fff6b5bdcc8dcc15569b33d3153cf31cd52
Archishmaan Peyyety <archishou@gmail.com>
2017-11-04 21:06:08 -0400

    moved to v3.5 have to test this version

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/FtcOpModeRegister.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/FtcOpModeRegister.java
@@ -31,21 +31,38 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
 
 package org.firstinspires.ftc.robotcontroller.internal;
 
-import com.google.blocks.ftcrobotcontroller.runtime.BlocksOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpModeManager;
 import com.qualcomm.robotcore.eventloop.opmode.OpModeRegister;
-import com.qualcomm.robotcore.eventloop.opmode.AnnotatedOpModeRegistrar;
-import com.qualcomm.robotcore.hardware.HardwareMap;
-
-import org.firstinspires.ftc.robotcore.internal.opmode.OpModeManagerImpl;
 
 /**
  * {@link FtcOpModeRegister} is responsible for registering opmodes for use in an FTC game.
  * @see #register(OpModeManager)
  */
 public class FtcOpModeRegister implements OpModeRegister {
+
+    /**
+     * {@link #register(OpModeManager)} is called by the SDK game in order to register
+     * OpMode classes or instances that will participate in an FTC game.
+     *
+     * There are two mechanisms by which an OpMode may be registered.
+     *
+     *  1) The preferred method is by means of class annotations in the OpMode itself.
+     *  See, for example the class annotations in {@link ConceptNullOp}.
+     *
+     *  2) The other, retired,  method is to modify this {@link #register(OpModeManager)}
+     *  method to include explicit calls to OpModeManager.register().
+     *  This method of modifying this file directly is discouraged, as it
+     *  makes updates to the SDK harder to integrate into your code.
+     *
+     * @param manager the object which contains methods for carrying out OpMode registrations
+     *
+     * @see com.qualcomm.robotcore.eventloop.opmode.TeleOp
+     * @see com.qualcomm.robotcore.eventloop.opmode.Autonomous
+     */
     public void register(OpModeManager manager) {
+
+        /**
+         * Any manual OpMode class registrations should go here.
+         */
     }
 }
