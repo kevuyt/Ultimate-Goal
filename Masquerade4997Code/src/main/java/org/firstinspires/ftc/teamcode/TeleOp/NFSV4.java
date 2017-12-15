2d8d85474229786aff418fde433459e119bcd4e1
Archishmaan Peyyety <archishou@gmail.com>
2017-12-14 20:10:41 -0500

    no need to reverse intake direcion

--- Masquerade4997Code/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/NFSV4.java
+++ Masquerade4997Code/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/NFSV4.java
@@ -61,7 +61,7 @@ public class NFSV4 extends MasqLinearOpMode implements Constants {
                 glyphBottomOpenState = false;
                 glyphTopOpenState = true;
                 bottomIntakePower = 1;
-                robot.bottomIntake.setPower(1);
+                robot.bottomIntake.setPower(0);
                 controller1.update();
             }
             if (controller2.xOnPress() && jewelArmInRed) {
