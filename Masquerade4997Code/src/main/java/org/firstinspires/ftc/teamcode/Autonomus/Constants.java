c542b70fc01d812e5ea20fd6f85d1cc5aedae040
Archishmaan Peyyety <archishou@gmail.com>
2018-04-23 12:23:05 -0400

    sample auto

---
+++ Masquerade4997Code/src/main/java/org/firstinspires/ftc/teamcode/Autonomus/Constants.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode.Autonomus;
+
+/**
+ * Created by Archish on 4/23/18.
+ */
+
+public interface Constants {
+}
---
+++ Masquerade4997Code/src/main/java/org/firstinspires/ftc/teamcode/Autonomus/Sample.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode.Autonomus;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import Library4997.MasqWrappers.MasqLinearOpMode;
+
+/**
+ * Created by Archish on 4/23/18.
+ */
+@Autonomous(name = "Sample", group = "Autonomus")
+public class Sample extends MasqLinearOpMode implements Constants {
+    public void runLinearOpMode() throws InterruptedException {
+        robot.mapHardware(hardwareMap);
+        while (!opModeIsActive()) {
+            dash.create(robot.imu);
+            dash.update();
+        }
+        waitForStart();
+        robot.sleep(robot.getDelay());
+    }
+}
