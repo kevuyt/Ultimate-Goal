72d57d5f0568309e254fa735df7b0b6d19c155d4
Archishmaan Peyyety <archishou@gmail.com>
2018-12-13 12:42:16 -0500

    saftey on hang

--- Masquerade4997Code/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/NFSV2.java
+++ Masquerade4997Code/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/NFSV2.java
@@ -39,9 +39,10 @@ public class NFSV2 extends MasqLinearOpMode implements Constants {
             if (controller2.b()) falcon.dumper.setPosition(DUMPER_OUT);
             else falcon.dumper.setPosition(DUMPER_IN);
 
-            if (controller2.leftStickY() > 0) falcon.hangSystem.setPower(-1);
-            else if (controller2.leftStickY() < 0) falcon.hangSystem.setPower(1);
+            if (controller2.leftStickY() > 0 && !falcon.limitBottom.isPressed()) falcon.hangSystem.setPower(-1);
+            else if (controller2.leftStickY() < 0 && !falcon.limitTop.isPressed()) falcon.hangSystem.setPower(1);
             else falcon.hangSystem.setPower(0);
+
             falcon.rotator.DriverControl(controller2);
             falcon.rotator.setLiftPosition(falcon.lift.getCurrentPosition());
 
