61c90d9759f5053075cf5e4a946a4d9c12d719b8
Archishmaan Peyyety <archishou@gmail.com>
2019-04-22 03:09:02 -0400

    remove 2018-2019 code.

--- Masquerade4997Code/src/main/java/org/firstinspires/ftc/teamcode/Robots/Reserection/TeleOp/Constants.java
+++
@@ -1,9 +0,0 @@
-package org.firstinspires.ftc.teamcode.Robots.Reserection.TeleOp;
-
-/**
- * This is an area you can add all your TeleOp constants for example the closed position of a certain servo etc.
- */
-public interface Constants {
-    double PARTICLE_DUMPER_IN = 0.6, PARTICLE_DUMPER_OUT = .85, PARTICLE_DUMPER_PARALLEL = .42;
-    double COLLECTOR_DUMPER_IN = 0.5, COLLECTOR_DUMPER_OUT = 0;
-}
--- Masquerade4997Code/src/main/java/org/firstinspires/ftc/teamcode/Robots/Reserection/Test/ConstantsProgrammer.java
+++ Masquerade4997Code/src/main/java/org/firstinspires/ftc/teamcode/Robots/Reserection/Test/ConstantsProgrammer.java
@@ -2,8 +2,6 @@ package org.firstinspires.ftc.teamcode.Robots.Reserection.Test;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-import org.firstinspires.ftc.teamcode.Robots.Reserection.Resurrection;
-
 import Library4997.MasqWrappers.MasqLinearOpMode;
 
 /**
@@ -12,31 +10,18 @@ import Library4997.MasqWrappers.MasqLinearOpMode;
  */
 @TeleOp(name = "ConstantsProgrammer", group = "NFS")
 public class ConstantsProgrammer extends MasqLinearOpMode {
-    private Resurrection falcon = new Resurrection();
     private double dumper = 0;
     @Override
     public void runLinearOpMode() throws InterruptedException {
-        falcon.mapHardware(hardwareMap);
         while (!opModeIsActive()) {
             dash.create("HELLO ");
             dash.update();
         }
         waitForStart();
         while (opModeIsActive()) {
-            if (controller1.xOnPress()) {
-                dumper += 0.01;
-            }
-            if (controller1.yOnPress()) {
-                dumper -= 0.01;
-            }
+            if (controller1.xOnPress()) dumper += 0.01;
+            if (controller1.yOnPress()) dumper -= 0.01;
             controller1.update();
-            falcon.particleDumper.setPosition(dumper);
-
-            falcon.scoreLift.DriverControl(controller1);
-
-            dash.create("Dumper: ", falcon.particleDumper.getPosition());
-            dash.update();
         }
-
     }
 }
