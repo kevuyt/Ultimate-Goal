863183afb2fd956fdc817792ad6f12cd52a4fd1c
Archishmaan Peyyety <archishou@gmail.com>
2019-12-22 12:31:34 -0500

    make a move
    Signed-off-by: Archishmaan Peyyety <archishou@gmail.com>

---
+++ Masquerade4997Library/src/main/java/Library4997/MasqCV/filters/MasqCVColorFilter.java
@@ -0,0 +1,11 @@
+package Library4997.MasqCV.filters;
+
+import org.opencv.core.Mat;
+
+/**
+ * Created by Victo on 1/1/2018.
+ */
+
+public abstract class MasqCVColorFilter {
+    public abstract void process(Mat input, Mat mask);
+}
