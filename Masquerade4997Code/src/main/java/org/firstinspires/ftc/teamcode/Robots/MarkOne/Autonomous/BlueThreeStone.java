6c849908b0b9230dfdf131d8953c7ad0bbed634e
Archishmaan Peyyety <archishou@gmail.com>
2020-01-29 12:56:06 -0500

    so close
    Signed-off-by: Archishmaan Peyyety <archishou@gmail.com>

--- Masquerade4997Code/src/main/java/org/firstinspires/ftc/teamcode/Robots/MarkOne/Autonomous/BlueThreeStone.java
+++ Masquerade4997Code/src/main/java/org/firstinspires/ftc/teamcode/Robots/MarkOne/Autonomous/BlueThreeStone.java
@@ -1,24 +1,18 @@
 package org.firstinspires.ftc.teamcode.Robots.MarkOne.Autonomous;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 
 import org.firstinspires.ftc.teamcode.Robots.MarkOne.Robot.MarkOne;
-import org.firstinspires.ftc.teamcode.Robots.MarkOne.Robot.SubSystems.CVInterpreter;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import Library4997.MasqControlSystems.MasqPurePursuit.MasqWayPoint;
-import Library4997.MasqResources.MasqHelpers.Direction;
 import Library4997.MasqResources.MasqMath.MasqPoint;
-import Library4997.MasqResources.MasqUtils;
 import Library4997.MasqWrappers.MasqLinearOpMode;
 
 import static Library4997.MasqControlSystems.MasqPurePursuit.MasqWayPoint.PointMode.MECH;
 import static org.firstinspires.ftc.teamcode.Robots.MarkOne.Robot.SubSystems.CVInterpreter.SkystonePosition;
-import static org.firstinspires.ftc.teamcode.Robots.MarkOne.Robot.SubSystems.CVInterpreter.SkystonePosition.LEFT;
-import static org.firstinspires.ftc.teamcode.Robots.MarkOne.Robot.SubSystems.CVInterpreter.SkystonePosition.MIDDLE;
 
 /**
  * Created by Keval Kataria on 1/4/2020
@@ -31,13 +25,15 @@ public class BlueThreeStone extends MasqLinearOpMode {
     private MasqWayPoint
             bridge1 = new MasqWayPoint().setPoint(-18, 18, -90).setSwitchMode(MECH),
             bridge2 = new MasqWayPoint().setPoint(-59, 22.5, -90),
-            foundation = new MasqWayPoint().setPoint(-92, 30, -90).setTargetRadius(3);
+            foundationOne = new MasqWayPoint().setPoint(-86, 30, -90).setTargetRadius(3).setMinVelocity(0),
+            foundationTwo = new MasqWayPoint().setPoint(-88, 30, -90).setTargetRadius(3).setMinVelocity(0),
+            foundationThree = new MasqWayPoint().setPoint(-92, 30, -90).setTargetRadius(3).setMinVelocity(0);
 
     @Override
     public void runLinearOpMode() throws InterruptedException {
         robot.init(hardwareMap);
         robot.initializeAutonomous();
-        robot.initCamera(hardwareMap);
+        //robot.initCamera(hardwareMap);
 
         stones.add(null);
 
@@ -51,8 +47,8 @@ public class BlueThreeStone extends MasqLinearOpMode {
 
 
         while (!opModeIsActive()) {
-            position = CVInterpreter.getPosition(robot.cv.detector);
-            dash.create("Skystone Position: ", position);
+            //position = CVInterpreter.getPosition(robot.cv.detector);
+            dash.create("Skystone Position: ");
             dash.update();
         }
 
@@ -64,7 +60,8 @@ public class BlueThreeStone extends MasqLinearOpMode {
         robot.sideGrabber.leftClose(0);
         robot.foundationHook.mid();
 
-
+        mainAuto(stones.get(1), stones.get(4),stones.get(2));
+        /*
         if (position == LEFT) runSimultaneously(
                 () -> mainAuto(stones.get(1), stones.get(4),stones.get(2)),
                 () -> robot.cv.stop()
@@ -74,21 +71,22 @@ public class BlueThreeStone extends MasqLinearOpMode {
                 () -> robot.cv.stop()
         );
         else runSimultaneously(
-                    () -> mainAuto(stones.get(3), stones.get(6),stones.get(1)),
-                    () -> robot.cv.stop()
-            );
+                () -> mainAuto(stones.get(3), stones.get(6),stones.get(1)),
+                () -> robot.cv.stop()
+        );
+        */
     }
 
     private void mainAuto(MasqWayPoint stone1, MasqWayPoint stone2, MasqWayPoint stone3) {
-        grabStone(stone1, true);
-        grabStone(stone2,false);
-        grabStone(stone3, false);
+        grabStone(stone1, foundationOne,true);
+        grabStone(stone2, foundationTwo,false);
+        grabStone(stone3, foundationThree,false);
         foundationPark();
     }
 
-    private void grabStone(MasqWayPoint stone, boolean firstStone) {
-        if (firstStone) robot.xyPath(4,stone);
-        else robot.xyPath(9,bridge2.setSwitchMode(MECH),bridge1, stone);
+    private void grabStone(MasqWayPoint stone, MasqWayPoint foundation, boolean firstStone) {
+        if (firstStone) robot.xyPath(4, stone);
+        else robot.xyPath(9, bridge2.setSwitchMode(MECH), bridge1, stone);
         robot.driveTrain.stopDriving();
         if (firstStone) robot.sideGrabber.rightDown(0.35);
         else robot.sideGrabber.rightDown(1);
@@ -103,21 +101,19 @@ public class BlueThreeStone extends MasqLinearOpMode {
 
     private void foundationPark() {
         sleep(0.25);
-        robot.turnAbsolute(179,1.5);
+        robot.turnAbsolute(170,1.5);
         robot.xyPath(2, new MasqWayPoint().setPoint(robot.tracker.getGlobalX(),
-                robot.tracker.getGlobalY()+5,robot.tracker.getHeading()).setMinVelocity(0).setTimeout(1));
+                robot.tracker.getGlobalY()+5,170).setMinVelocity(0).setTimeout(1));
         robot.foundationHook.lower();
         sleep();
-        MasqUtils.xySpeedController.setKp(0.08);
-        MasqUtils.xyAngleController.setKp(0.06);
-        MasqWayPoint p1 = new MasqWayPoint().setPoint(new MasqPoint(-80, -10, 30)).setMinVelocity(0.9)
-                .setTargetRadius(0.5).setModeSwitchRadius(5);
-        MasqWayPoint p2 = new MasqWayPoint().setPoint(new MasqPoint(-80, -5, 0)).setMinVelocity(0.9);
-        robot.xyPath(2.5, p1, p2);
-        MasqUtils.xySpeedController.setKp(0.045);
-        MasqWayPoint park = new MasqWayPoint().setPoint(-45,22,90);
+        MasqWayPoint p1 = new MasqWayPoint()
+                .setPoint(new MasqPoint(-75, 5, 90))
+                .setMinVelocity(0.5)
+                .setModeSwitchRadius(5);
+        robot.xyPath(2.5, p1);
         robot.foundationHook.raise();
         sleep();
+        MasqWayPoint park = new MasqWayPoint().setPoint(-45,22,90);
         robot.xyPath(2, park);
     }
 }
