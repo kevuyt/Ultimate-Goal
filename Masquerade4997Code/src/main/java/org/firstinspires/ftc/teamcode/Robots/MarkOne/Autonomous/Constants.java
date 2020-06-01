d40c21c143a53b8f93ec0840ac53f3f3a1d94869
Keval Kataria <kevalkataria@gmail.com>
2020-06-01 19:04:13 -0400

    deleting Skystone code
    Signed-off-by: Keval Kataria <kevalkataria@gmail.com>

--- Masquerade4997Code/src/main/java/org/firstinspires/ftc/teamcode/Robots/MarkOne/Autonomous/Constants.java
+++
@@ -1,9 +0,0 @@
-package org.firstinspires.ftc.teamcode.Robots.MarkOne.Autonomous;
-
-/**
- * Created by Archishmaan Peyyety on 2020-02-27.
- * Project: MasqLib
- */
-public interface Constants {
-    double STONE_DISTANCE = 28.5;
-}
--- Masquerade4997Code/src/main/java/org/firstinspires/ftc/teamcode/Robots/MarkOne/Robot/Constants.java
+++
@@ -1,9 +0,0 @@
-package org.firstinspires.ftc.teamcode.Robots.MarkOne.Robot;
-
-/**
- * Created by Archishmaan Peyyety on 2020-01-01.
- * Project: MasqLib
- */
-public interface Constants {
-    double aThing = 0;
-}
