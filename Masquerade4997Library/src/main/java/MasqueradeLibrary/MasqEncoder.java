e45177c84455ef233d73a1e42b2be5f5e81a6625
Keval Kataria <kevalkataria@gmail.com>
2021-03-15 19:26:04 -0400

    Motors should be done
    Signed-off-by: Keval Kataria <kevalkataria@gmail.com>

--- Masquerade4997Library/src/main/java/MasqueradeLibrary/MasqEncoder.java
+++
@@ -1,7 +0,0 @@
-package MasqueradeLibrary;
-
-/**
- * Created by Keval Kataria on 3/14/2021
- */
-public class MasqEncoder {
-}
---
+++ Masquerade4997Library/src/main/java/MasqueradeLibrary/MasqMotorSystem.java
@@ -0,0 +1,42 @@
+package MasqueradeLibrary;
+
+import androidx.annotation.NonNull;
+
+import static java.util.Locale.US;
+
+/**
+ * Created by Keval Kataria on 3/15/2021
+ */
+public class MasqMotorSystem {
+    MasqMotor[] motors;
+    String name;
+
+    public MasqMotorSystem(String name, MasqMotor... motors) {
+        this.motors = motors;
+        this.name = name;
+    }
+
+    public void resetEncoders() {for (MasqMotor masqMotor : motors) masqMotor.resetEncoder();}
+
+    public double getInches () {
+        double sum = 0;
+        for (MasqMotor masqMotor : motors) sum += masqMotor.getInches();
+        return sum / motors.length;
+    }
+
+    public void setPower(double power) {
+        for (MasqMotor masqMotor : motors) masqMotor.setPower(power);
+    }
+    public void setLimits (MasqTouchSensor min, MasqTouchSensor max) {
+        for (MasqMotor masqMotor : motors) masqMotor.setLimits(min, max);
+    }
+    public void setVelocityControl (boolean velocityControl) {
+        for (MasqMotor masqMotor : motors) masqMotor.setVelocityControl(velocityControl);
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return String.format(US, "%s:\nNum Motors: %d\nInches: %f", name, motors.length, getInches());
+    }
+}
