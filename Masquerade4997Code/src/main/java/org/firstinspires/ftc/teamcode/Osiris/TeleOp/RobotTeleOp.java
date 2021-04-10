b778983107f3e225581f4d2c87d880d600063c17
Keval Kataria <kevalkataria@gmail.com>
2021-04-10 18:32:16 -0400

    Added 3 shot in teleop and made some minor changes
    Signed-off-by: Keval Kataria <kevalkataria@gmail.com>

--- Masquerade4997Code/src/main/java/org/firstinspires/ftc/teamcode/Osiris/TeleOp/RobotTeleOp.java
+++ Masquerade4997Code/src/main/java/org/firstinspires/ftc/teamcode/Osiris/TeleOp/RobotTeleOp.java
@@ -17,8 +17,7 @@ public class RobotTeleOp extends MasqLinearOpMode {
     private final Osiris robot = new Osiris();
     private String mode = "GOAL";
     private double shooterPower = 0.5;
-    private boolean enabled = false;
-    private Thread thread = new Thread();
+    private Thread thread = new Thread(), thread2 = new Thread();
 
     @Override
     public void runLinearOpMode() {
@@ -32,10 +31,6 @@ public class RobotTeleOp extends MasqLinearOpMode {
         while(opModeIsActive()) {
             robot.MECH();
 
-            if(!enabled && gamepad1.left_trigger > 0) robot.intake.setPower(1);
-            else if(!enabled && gamepad1.right_trigger > 0) robot.intake.setPower(-1);
-            else robot.intake.setPower(0);
-
             if(gamepad1.left_bumper) {
                 robot.shooter.setPower(shooterPower);
                 robot.hopper.setPosition(1);
@@ -47,40 +42,65 @@ public class RobotTeleOp extends MasqLinearOpMode {
                     });
                     thread.start();
                 }
-                enabled = true;
+                if(gamepad1.y) {
+                    if (!thread2.isAlive()) {
+                        thread2 = new Thread(this::shoot);
+                        thread2.start();
+                    }
+                }
+                else if(gamepad1.right_bumper) robot.flicker.setPosition(1);
+                else if(!thread2.isAlive()) robot.flicker.setPosition(0);
+                robot.intake.setPower(0);
             }
             else {
                 robot.shooter.setPower(0);
                 robot.hopper.setPosition(0);
                 if(thread.isAlive()) thread.interrupt();
+                if(thread2.isAlive()) thread2.interrupt();
                 robot.compressor.setPosition(0);
-                enabled = false;
                 robot.claw.driverControl(gamepad1);
+                robot.flicker.setPosition(0);
+                if(gamepad1.left_trigger > 0) robot.intake.setPower(1);
+                else if(gamepad1.right_trigger > 0) robot.intake.setPower(-1);
+                else robot.intake.setPower(0);
             }
 
-            if(gamepad1.right_bumper && enabled) robot.flicker.setPosition(1);
-            else robot.flicker.setPosition(0);
-
-            /*if(gamepad1.dpad_left) {
+            if(gamepad1.dpad_left) {
                 mode = "POWER_SHOT";
-                shooterPower = 0.426;
+                shooterPower = 0.79;
             }
             else if(gamepad1.dpad_right) {
                 mode = "GOAL";
-                shooterPower = 0.5;
+                shooterPower = 1;
             }
-            else if(gamepad1.dpad_up) {
-                mode = "HIGH_POWER";
-                shooterPower = 0.55;
-            }*/
+            else if(gamepad2.dpad_up) {
+                mode = "MIDDLE";
+                shooterPower = 0.90;
+            }
 
-            if(gamepad1.dpad_left) shooterPower += 0.01;
-            else if(gamepad1.dpad_right) shooterPower -= 0.01;
+            /*
+            if(gamepad1.dpad_left) shooterPower -= 0.001;
+            if(gamepad1.dpad_right) shooterPower += 0.001;
+            */
 
-            //dash.create("Shooter Mode:", mode);
-            dash.create("Shooter Power:", shooterPower);
+            dash.create("Shooter Mode:", mode);
+            //dash.create(robot.shooter);
             dash.create("Rings in Hopper:", robot.getRings());
             dash.update();
         }
+    }
+    public void shoot() {
+        robot.flicker.setPosition(1);
+        sleep();
+        robot.flicker.setPosition(0);
+        sleep();
+        robot.flicker.setPosition(1);
+        sleep();
+        robot.flicker.setPosition(0);
+        sleep();
+        robot.flicker.setPosition(1);
+        sleep();
+        robot.flicker.setPosition(0);
+        sleep();
     }
 }
